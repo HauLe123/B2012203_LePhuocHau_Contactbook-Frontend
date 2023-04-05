91e1b8eb1ec4d0429b150c2819ad6001adffaea9
Hau Le <113987147+HauLe123@users.noreply.github.com>
2023-04-05 11:39:00 +0700

    Update ContactAdd.vue

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -7,7 +7,7 @@
 </template>
 <script>
 import ContactForm from "@/components/ContactForm.vue";
-import ContactService from "@/services/contact.services";
+import ContactService from "@/services/contact.service  ";
 export default {
     components: {
         ContactForm,
